18df65c89b759d83913899ce2eaf4a4dc9c40074
zuoliangyu <295871597@qq.com>
2024-02-15 14:24:18 +0800

    准备重写第十四届省赛，新增模板代码方便github观看

---
+++ demo_zuolan_old/Driver/Uart.h
@@ -0,0 +1,3 @@
+#include <STC15F2K60S2.H>
+#include <stdio.h>
+void UartInit(void); // 9600bps@12.000MHz
--- 真题/14 第十四届省赛/test_1_20/Driver/onewire.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ONEWIRE_H
-#define __ONEWIRE_H
-
-
-
-float rd_temperature(void);
-
-#endif
